6d47cea41a6c3b1c1efb274c14f2b5d243e32a6b
facundomdelp <facundomdelp@gmail.com>
2022-12-31 13:10:46 -0300

    probando

--- deploy.sh
+++ deploy.sh
@@ -16,12 +16,14 @@ echo > .nojekyll
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -B main
+git checkout -B master
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:facundomdelp/parakalo.git main:gh-pages
+git push -f git@github.com:facundomdelp/parakalo.git master:gh-pages
+
+cd -
