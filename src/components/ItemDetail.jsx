a224feb1af13ac9edb852fbd467fd466ab25c06a
facundomdelp <facundomdelp@gmail.com>
2023-01-17 22:10:03 -0300

    delete hook

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,60 +1,44 @@
 import {
-  Box,
   Card,
   CardBody,
+  Flex,
   Heading,
   Image,
-  Spinner,
   Stack,
   Text,
 } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import ItemCount from './ItemCount'
 
-const ItemDetail = ({ singleProduct }) => {
-  const [loading, setLoading] = useState(true)
-  console.log('🚀 ~ ItemDetail ~ loading', loading)
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(!loading)
-    }, 2000)
-  }, [])
+const ItemDetail = ({ products }) => {
+  const { nombre } = useParams()
+  const singleProduct = products.find(
+    (singleProduct) => singleProduct.nombre === nombre
+  )
   return (
-    <>
-      {loading ? (
-        <Spinner
-          thickness='4px'
-          speed='0.65s'
-          emptyColor='gray.200'
-          color='blue.500'
-          size='xl'
-          textAlign='center'
-          marginBlock='10em'
-        />
-      ) : (
-        <Card boxShadow='2xl' maxW='20em'>
-          <CardBody>
-            <Heading size='md'>{singleProduct.nombre}</Heading>
-            <Image
-              src={singleProduct.imagen}
-              alt={singleProduct.nombre}
-              borderRadius='lg'
-              boxSize='12em'
-              m='1em'
-            />
-            <Stack mt='6' spacing='3'>
-              <Text fontSize='sm'>{singleProduct.descripcion}</Text>
-              <Text color='blue.600' fontSize='2xl'>
-                {singleProduct.precio}
-              </Text>
-            </Stack>
-            <Box marginInline='1em' marginTop='1em'>
-              <ItemCount singleProduct={singleProduct} />
-            </Box>
-          </CardBody>
-        </Card>
-      )}
-    </>
+    <Flex justifyContent='center' marginTop='2em'>
+      <Card boxShadow='2xl' maxW='20em'>
+        <CardBody>
+          <Heading size='md' color='gray.700'>
+            {singleProduct.nombre}
+          </Heading>
+          <Image
+            src={singleProduct.imagen}
+            alt={singleProduct.nombre}
+            borderRadius='lg'
+            boxSize='12em'
+            m='1em'
+          />
+          <Stack mt='6' spacing='3'>
+            <Text fontSize='sm'>{singleProduct.descripcion}</Text>
+            <Text color='blue.600' fontSize='2xl'>
+              {singleProduct.precio}
+            </Text>
+          </Stack>
+        </CardBody>
+        <ItemCount singleProduct={singleProduct} />
+      </Card>
+    </Flex>
   )
 }
 
