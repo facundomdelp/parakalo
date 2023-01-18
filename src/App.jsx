c0903c4d9056f479894b12525589c87254334e75
facundomdelp <facundomdelp@gmail.com>
2023-01-17 22:08:39 -0300

    add routes and hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
+import Greetings from './components/Greetings'
+import ItemDetailContainer from './components/ItemDetailContainer'
 import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
+import './scrollbar.css'
+import { Flex, Spinner } from '@chakra-ui/react'
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const greetings = '¡Bienvenidos a la tienda Parakaló!'
+  const categoria = useLocation().pathname.split('/')[1]
+  const URLJSON = '/data.json'
+  console.log('🚀 ~ App ~ URLJSON', URLJSON)
+  console.log('🚀 ~ App ~ categoria', categoria)
+  console.log('🚀 ~ App ~ loading', loading)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      fetch(URLJSON)
+        .then((products) => products.json())
+        .then((products) => setProducts(products.cortinas))
+        .then((loading) => setLoading(false))
+    }, 2000)
+  }, [categoria])
   return (
     <React.Fragment>
       <NavBar />
-      <Routes>
-        <Route path='/' element={<ItemListContainer categoria='todas' />} />
-        <Route
-          path='/estandar'
-          element={<ItemListContainer categoria='estandar' />}
-        />
-        <Route path='/arte' element={<ItemListContainer categoria='arte' />} />
-        <Route
-          path='/chicos'
-          element={<ItemListContainer categoria='chicos' />}
-        />
-        <Route path='*' element={<Navigate to='/' />} />
-      </Routes>
+      <Greetings greetings={greetings} />
+      {loading ? (
+        <Flex justifyContent='center' mt='1em'>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </Flex>
+      ) : (
+        <Routes>
+          <Route path='/' element={<ItemListContainer products={products} />} />
+          <Route
+            path='/estandar'
+            element={<ItemListContainer products={products} />}
+          />
+          <Route
+            path='/arte'
+            element={<ItemListContainer products={products} />}
+          />
+          <Route
+            path='/chicos'
+            element={<ItemListContainer products={products} />}
+          />
+          <Route
+            path='/detalle/:nombre'
+            element={<ItemDetailContainer products={products} />}
+          />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      )}
     </React.Fragment>
   )
 }
