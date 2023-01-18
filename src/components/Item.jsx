56f5c3e988c20ab054f5c6cac67f3ce48e3faba9
facundomdelp <facundomdelp@gmail.com>
2023-01-17 22:09:42 -0300

    delete hook and modal

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,22 +3,15 @@ import {
   Card,
   CardBody,
   Divider,
-  Flex,
   Heading,
   Image,
-  Modal,
-  ModalCloseButton,
-  ModalContent,
-  ModalOverlay,
   Stack,
   Text,
-  useDisclosure,
 } from '@chakra-ui/react'
 import React from 'react'
-import ItemDetail from './ItemDetail'
+import { Link } from 'react-router-dom'
 
 const Item = ({ singleProduct }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <Card maxW='250px' boxShadow='2xl'>
       <CardBody>
@@ -29,20 +22,14 @@ const Item = ({ singleProduct }) => {
           boxSize='12em'
         />
         <Stack mt='6' spacing='3'>
-          <Heading size='sm'>{singleProduct.nombre}</Heading>
-          <Button colorScheme='gray' fontSize='14px' onClick={onOpen}>
-            Ver detalle
-          </Button>
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent maxW='20em'>
-              <Flex justifyContent='center'>
-                {/* ModalContent equivale a ItemListContainer */}
-                <ItemDetail singleProduct={singleProduct} />
-              </Flex>
-              <ModalCloseButton />
-            </ModalContent>
-          </Modal>
+          <Heading size='sm' color='gray.800'>
+            {singleProduct.nombre}
+          </Heading>
+          <Link to={`/detalle/${singleProduct.nombre}`}>
+            <Button colorScheme='gray' fontSize='14px' w='100%'>
+              Ver detalle
+            </Button>
+          </Link>
           <Text color='blue.600' fontSize='2xl'>
             {singleProduct.precio}
           </Text>
