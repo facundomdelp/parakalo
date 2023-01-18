c26dd73f7e4c407b0456d67876a912db1d4fce38
facundomdelp <facundomdelp@gmail.com>
2023-01-17 22:10:11 -0300

    create

---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ItemDetail from './ItemDetail'
+
+const ItemDetailContainer = ({ products }) => {
+  return <ItemDetail products={products} />
+}
+
+export default ItemDetailContainer
