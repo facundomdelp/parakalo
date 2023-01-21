c0b6728a029eb96dd6802703f2540cc525d0fb5a
facundomdelp <facundomdelp@gmail.com>
2023-01-21 19:31:42 -0300

    agregar key a map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,12 +14,12 @@ import { Link } from 'react-router-dom'
 import SingleLink from './SingleLink'
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
 
-export default function Simple() {
+export default function NavBar() {
   const secciones = [
-    { to: '/', singleLink: 'Productos' },
-    { to: 'estandar', singleLink: 'Estándar' },
-    { to: 'arte', singleLink: 'Arte' },
-    { to: 'chicos', singleLink: 'Niños' },
+    { id: 1, to: '/', singleLink: 'Productos' },
+    { id: 2, to: 'estandar', singleLink: 'Estándar' },
+    { id: 3, to: 'arte', singleLink: 'Arte' },
+    { id: 4, to: 'chicos', singleLink: 'Niños' },
   ]
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
@@ -50,7 +50,7 @@ export default function Simple() {
               display={{ base: 'none', md: 'flex' }}
             >
               {secciones.map((seccion) => (
-                <Link to={seccion.to}>
+                <Link key={seccion.id} to={seccion.to}>
                   <SingleLink singleLink={seccion.singleLink} />
                 </Link>
               ))}
@@ -64,7 +64,7 @@ export default function Simple() {
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
               {secciones.map((seccion) => (
-                <Link to={seccion.to}>
+                <Link key={seccion.id} to={seccion.to}>
                   <SingleLink singleLink={seccion.singleLink} />
                 </Link>
               ))}
