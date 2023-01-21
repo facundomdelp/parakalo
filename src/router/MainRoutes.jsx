720760327b7ab9dc9786fdd90130ad5765da0e26
facundomdelp <facundomdelp@gmail.com>
2023-01-21 19:31:55 -0300

    agregar ruta a carrito

--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ItemListContainer from '../components/ItemListContainer'
 import ItemDetailContainer from '../components/ItemDetailContainer'
+import Cart from '../components/Cart'
 
 const MainRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const MainRoutes = () => {
       <Route path={'/'} element={<ItemListContainer />} />
       <Route path={'/:categoria'} element={<ItemListContainer />} />
       <Route path={'/detalle/:nombre'} element={<ItemDetailContainer />} />
+      <Route path={'/carrito'} element={<Cart />} />
       <Route path={'*'} element={<Navigate to={'/'} />} />
     </Routes>
   )
