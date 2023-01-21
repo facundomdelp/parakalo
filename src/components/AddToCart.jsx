a79f782b3bf54adfc95c7519f3b4468f70a54e6d
facundomdelp <facundomdelp@gmail.com>
2023-01-21 19:30:21 -0300

    agregar funcion a boton de compra

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,23 +1,55 @@
-import { Button, Icon } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Flex, Icon } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import Stock from './Stock'
 
-const AddToCart = () => {
+const AddToCart = ({ cantidadAgregada, stock }) => {
+  const [cantidadComprada, setCantidadComprada] = useState(0)
+  const onAdd = () => {
+    setCantidadComprada(cantidadComprada + cantidadAgregada)
+    return cantidadComprada
+  }
+  console.log(cantidadComprada)
+
   return (
-    <Button
-      borderWidth={'.5px'}
-      borderColor={'gray.400'}
-      w={'80%'}
-      h={'2.5em'}
-      textAlign={'center'}
-      color={'gray.700'}
-      marginInline={'auto'}
-      marginTop={'.5em'}
-      marginBottom={'1.5em'}
-    >
-      AGREGAR AL CARRITO
-      <Icon ml={2} as={AiOutlineShoppingCart} />
-    </Button>
+    <React.Fragment>
+      {cantidadComprada !== 0 ? (
+        <Link to={'/carrito'}>
+          <Flex justifyContent={'center'}>
+            <Button
+              borderWidth={'.5px'}
+              borderColor={'gray.400'}
+              w={'80%'}
+              h={'2.5em'}
+              textAlign={'center'}
+              color={'gray.700'}
+              marginTop={'.5em'}
+              marginBottom={'1.5em'}
+            >
+              FINALIZAR MI COMPRA
+            </Button>
+          </Flex>
+        </Link>
+      ) : (
+        <Button
+          borderWidth={'.5px'}
+          borderColor={'gray.400'}
+          w={'80%'}
+          h={'2.5em'}
+          textAlign={'center'}
+          color={'gray.700'}
+          marginInline={'auto'}
+          marginTop={'.5em'}
+          marginBottom={'1.5em'}
+          onClick={onAdd}
+        >
+          AGREGAR AL CARRITO
+          <Icon ml={2} as={AiOutlineShoppingCart} />
+        </Button>
+      )}
+      <Stock stock={stock} />
+    </React.Fragment>
   )
 }
 
