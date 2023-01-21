e9db538f5def306d13e071713724950794935673
facundomdelp <facundomdelp@gmail.com>
2023-01-21 10:27:12 -0300

    se agrega el saludo como constante dentro del componente

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Box, Text } from '@chakra-ui/react'
 
-const Greetings = ({ greetings }) => {
+const Greetings = () => {
+  const greetings = '¡Bienvenidos a la tienda Parakaló!'
   return (
     <Box w='100%' bg='#FBF0DA'>
       <Text align='center' fontWeight='500'>
