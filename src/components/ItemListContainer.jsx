08b844b42c2445a7a8ff749b6ad8a806350b3cb9
facundomdelp <facundomdelp@gmail.com>
2023-01-21 10:28:37 -0300

    llamar api e importar loading en este componente

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,24 +1,36 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Grid } from '@chakra-ui/react'
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import Item from './Item'
+import getProducts from './../customHooks/getProducts'
+import Loading from './Loading'
 
-function ItemListContainer({ products }) {
+function ItemListContainer() {
   const categoria = useLocation().pathname.split('/')[1]
+  const [products, loading] = getProducts()
   return (
-    <React.Fragment>
-      <Flex flexWrap='wrap' justifyContent='center' gap='1.5em' p='1.5em'>
-        {categoria === ''
-          ? products.map((singleProduct) => (
+    <Box>
+      <Grid
+        gridTemplateColumns='repeat(auto-fit, minmax(250px, 1fr))'
+        gap='1rem'
+        margin='1rem'
+        textAlign='center'
+      >
+        {loading ? (
+          <Loading />
+        ) : categoria === '' ? (
+          products.map((singleProduct) => (
+            <Item key={singleProduct.id} singleProduct={singleProduct} />
+          ))
+        ) : (
+          products
+            .filter((singleProduct) => singleProduct.categoria === categoria)
+            .map((singleProduct) => (
               <Item key={singleProduct.id} singleProduct={singleProduct} />
             ))
-          : products
-              .filter((singleProduct) => singleProduct.categoria === categoria)
-              .map((singleProduct) => (
-                <Item key={singleProduct.id} singleProduct={singleProduct} />
-              ))}
-      </Flex>
-    </React.Fragment>
+        )}
+      </Grid>
+    </Box>
   )
 }
 
