70fd753e880d1c92490f3092eb7097945f87c580
facundomdelp <facundomdelp@gmail.com>
2023-01-22 21:09:25 -0300

    agregar ruta

--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ItemListContainer from '../components/ItemListContainer'
 import ItemDetailContainer from '../components/ItemDetailContainer'
-import Cart from '../components/Cart'
+import CartDetail from '../components/CartDetail'
 
 const MainRoutes = () => {
   return (
@@ -10,7 +10,7 @@ const MainRoutes = () => {
       <Route path={'/'} element={<ItemListContainer />} />
       <Route path={'/:categoria'} element={<ItemListContainer />} />
       <Route path={'/detalle/:nombre'} element={<ItemDetailContainer />} />
-      <Route path={'/carrito'} element={<Cart />} />
+      <Route path={'/carrito'} element={<CartDetail />} />
       <Route path={'*'} element={<Navigate to={'/'} />} />
     </Routes>
   )
