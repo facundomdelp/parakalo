05ad466adb7a2fcb0e269d5bd8264473ca21a478
facundomdelp <facundomdelp@gmail.com>
2023-01-22 21:08:29 -0300

    agregar funcionalidad a onAdd

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,37 +1,25 @@
-import { Button, Flex, Icon } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { Button, Icon } from '@chakra-ui/react'
+import React, { useContext } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
-import Stock from './Stock'
+import CartContext from '../context/CartContext'
 
-const AddToCart = ({ cantidadAgregada, stock }) => {
-  const [cantidadComprada, setCantidadComprada] = useState(0)
-  const onAdd = () => {
-    setCantidadComprada(cantidadComprada + cantidadAgregada)
-    return cantidadComprada
+const AddToCart = ({ id, nombre, precio, cantidadAgregada }) => {
+  const { carrito, setCarrito } = useContext(CartContext)
+  function onAdd() {
+    if (carrito.find((compra) => compra.id === id)) {
+      carrito.find((compra) => compra.id === id).cantidad += cantidadAgregada
+      setCarrito([...carrito])
+    } else {
+      setCarrito([
+        ...carrito,
+        { id: id, nombre: nombre, precio: precio, cantidad: cantidadAgregada },
+      ])
+    }
   }
-  console.log(cantidadComprada)
-
+  console.log('🚀 ~ AddToCart ~ carrito', carrito)
   return (
     <React.Fragment>
-      {cantidadComprada !== 0 ? (
-        <Link to={'/carrito'}>
-          <Flex justifyContent={'center'}>
-            <Button
-              borderWidth={'.5px'}
-              borderColor={'gray.400'}
-              w={'80%'}
-              h={'2.5em'}
-              textAlign={'center'}
-              color={'gray.700'}
-              marginTop={'.5em'}
-              marginBottom={'1.5em'}
-            >
-              FINALIZAR MI COMPRA
-            </Button>
-          </Flex>
-        </Link>
-      ) : (
+      {
         <Button
           borderWidth={'.5px'}
           borderColor={'gray.400'}
@@ -47,8 +35,7 @@ const AddToCart = ({ cantidadAgregada, stock }) => {
           AGREGAR AL CARRITO
           <Icon ml={2} as={AiOutlineShoppingCart} />
         </Button>
-      )}
-      <Stock stock={stock} />
+      }
     </React.Fragment>
   )
 }
