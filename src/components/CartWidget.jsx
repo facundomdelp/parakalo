10e69d2accf36d653f7673c8572cc252c018902b
facundomdelp <facundomdelp@gmail.com>
2023-01-26 19:22:49 -0300

    sincronizar widget carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,23 +1,34 @@
 import { Badge, Flex, Icon } from '@chakra-ui/react'
+import { useContext } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import CartContext from '../context/CartContext'
 
 const CartWidget = () => {
+  const { carrito } = useContext(CartContext)
+  const sumaDeCompras =
+    carrito > 0
+      ? carrito.reduce(
+          (acumulador, valorActual) =>
+            acumulador.cantidad + valorActual.cantidad
+        )
+      : 0
   return (
     <Flex>
       <Icon as={AiOutlineShoppingCart} boxSize={8} mr={2} />
       <Badge
-        textAlign={'center'}
-        pt={'1.25'}
-        ml={'-3.5'}
-        mt={'4'}
-        h={'5'}
-        w={'4'}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        ml={-4}
+        mt={4}
+        h={5}
+        w={5}
         color={'white'}
-        borderRadius={'lg'}
+        borderRadius={'50%'}
         fontSize={'0.7em'}
         bg={'rgba(106, 44, 112)'}
       >
-        8
+        {sumaDeCompras}
       </Badge>
     </Flex>
   )
