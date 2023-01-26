2c0a7c31dc8a51756bd248c9f9fe810380d5943f
facundomdelp <facundomdelp@gmail.com>
2023-01-26 19:22:32 -0300

    desarrollar tabla

--- src/components/CartDetail.jsx
+++ src/components/CartDetail.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import React, { useContext } from 'react'
+import CartContext from '../context/CartContext'
+import { Box, Button, Table, Td, Th, Tr } from '@chakra-ui/react'
 
 const CartDetail = () => {
-  return <Box minH={'100vh'}>CARRITO</Box>
+  const { carrito } = useContext(CartContext)
+  return (
+    <Box minH={'100vh'}>
+      <Table>
+        <Tr>
+          <Th>Producto</Th>
+          <Th textAlign={'center'}>Cantidad</Th>
+          <Th textAlign={'center'}>Precio por Artículo</Th>
+          <Th textAlign={'center'}>Precio Final</Th>
+        </Tr>
+        {carrito.map((compra) => (
+          <Tr key={compra.id}>
+            <Td>{compra.nombre}</Td>
+            <Td textAlign={'center'}>{compra.cantidad}</Td>
+            <Td textAlign={'center'}>{compra.precio}</Td>
+            <Td textAlign={'center'}>
+              ${compra.cantidad * compra.precio.slice(1)}
+            </Td>
+            <Td>
+              <Button>ELIMINAR</Button>
+            </Td>
+          </Tr>
+        ))}
+      </Table>
+    </Box>
+  )
 }
 
 export default CartDetail
