40166231258f450069b1476b8eb61920dc159fa5
facundomdelp <facundomdelp@gmail.com>
2023-01-29 18:57:11 -0300

    agregar utilidades para el scrolling

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './hooks/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </ChakraProvider>
