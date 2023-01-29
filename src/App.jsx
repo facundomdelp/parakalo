573ce67d19dd51d0f9d4d8a250af8f8dc7390e4b
facundomdelp <facundomdelp@gmail.com>
2023-01-29 13:53:24 -0300

    instalar y configurar firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Greetings from './components/Greetings'
 import NavBar from './components/NavBar'
 import './scrollbar.css'
 import MainRoutes from './router/MainRoutes'
 import Footer from './components/Footer'
 import { CartContextProvider } from './context/CartContext'
+import { collection, getDocs, doc } from 'firebase/firestore'
+import { db } from './../db/firebase-config'
 
 function App() {
+  const [items, setItems] = useState([])
+  const itemsCollectionRef = collection(db, 'products')
+
+  const getItems = async () => {
+    const querySnapshot = await getDocs(itemsCollectionRef)
+    const docs = querySnapshot.docs.map((doc) => doc.data())
+    setItems(docs)
+  }
+
+  console.log(items)
+
+  useEffect(() => {
+    getItems()
+  }, [])
+
   return (
     <React.Fragment>
       <CartContextProvider>
