50c4e12d5af5464f9c2158743c02c2c4ad3ba051
facundomdelp <facundomdelp@gmail.com>
2023-01-31 20:35:48 -0300

    limpiar app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Greetings from './components/Greetings'
 import NavBar from './components/NavBar'
 import './scrollbar.css'
 import MainRoutes from './router/MainRoutes'
 import Footer from './components/Footer'
 import { CartContextProvider } from './context/CartContext'
-import { collection, getDocs } from 'firebase/firestore'
-import { db } from './../db/firebase-config'
 
 function App() {
-  const [products, setProducts] = useState([])
-  const itemsCollectionRef = collection(db, 'products')
-
-  const getProducts = async () => {
-    const querySnapshot = await getDocs(itemsCollectionRef)
-    setProducts(querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id})))
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [])
-
-  console.log('🚀 ~ products', products)
 
   return (
     <React.Fragment>
