c91c0ee70999ffc62acf1a5bd8ce9a3039ed87af
facundomdelp <facundomdelp@gmail.com>
2023-02-05 13:34:21 -0300

    cambiar path de importacion

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from './hooks/ScrollToTop'
+import ScrollToTop from './utils/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
